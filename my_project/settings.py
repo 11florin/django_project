932c908793e3280fb3687a4816bd568dd1f47d1c
Florin Albu <albuflorin929@yahoo.co.uk>
2026-08-03 11:04:06 +0100

    Add setting and Procfile and install gunicorn for deploy with heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9zi^%*$!2t7$h)4+^qi&9jk(_2-xb*u@77zh_bfa-2^cthhlsy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
